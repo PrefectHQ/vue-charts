0970b7a08d5342416d0b3c33c6e00504853a13a9
Craig Harshbarger <pleek91@gmail.com>
2022-04-20 14:31:43 -0500

    Update demo

--- demo/views/Heatmap.vue
+++ demo/views/Heatmap.vue
@@ -1,19 +1,29 @@
 <template>
-  <Heatmap class="heatmap" :items="items" />
+  <div class="controls">
+    <span>BucketAmount</span>
+    <input v-model="bucketAmount" type="number">
+    <button type="button" @click="add">
+      Add Item
+    </button>
+  </div>
+  <Heatmap class="heatmap" v-bind="{ items, bucketAmount }" />
 </template>
 
 <script lang="ts" setup>
+  import { ref, Ref } from 'vue'
   import Heatmap from '../../src/components/Heatmap.vue'
   import { HeatmapItem } from '../../src/types'
 
   const oneWeekAgo = new Date(new Date().setDate(new Date().getDate() - 7))
   const now = new Date()
-  const items: HeatmapItem[] = new Array(50).fill(null).map(getHeatmapItem)
+  const items: Ref<HeatmapItem[]> = ref(new Array(50).fill(null).map(getHeatmapItem))
+  const bucketAmount = ref(30)
 
   function getHeatmapItem(): HeatmapItem {
     return {
       id: getRandomString(),
       date: getRandomDate(oneWeekAgo, now),
+      itemClass: 'test-item',
     }
   }
 
@@ -24,11 +34,19 @@
   function getRandomDate(start: Date, end: Date): Date {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
   }
+
+  function add(): void {
+    items.value.push(getHeatmapItem())
+  }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .heatmap {
-height: min-content;
-padding: 20px;
+  height: min-content;
+  padding: 20px;
+}
+
+.test-item {
+  background-color: blue;
 }
 </style>
