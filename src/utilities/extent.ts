10c47a0079b45b1b7f9b7ab8cc26a9a53618fcfe
Craig Harshbarger <pleek91@gmail.com>
2022-04-21 17:14:17 -0500

    Fix the getDateExtend utility so it always returns a range

--- src/utilities/extent.ts
+++ src/utilities/extent.ts
@@ -7,11 +7,13 @@ export function extentUndefined(extent: unknown[]): extent is [undefined, undefi
 
 export function getDateExtent<T extends Record<string, unknown>>(items: T[], accessor: DateExtentAccessor<T>): [Date, Date] {
   const response = extent(items, accessor)
+  const [start, end] = response
 
-  if (extentUndefined(response)) {
+  if (extentUndefined(response) || start == end) {
     const dateNow = new Date
-    const offset = dateNow.setDate(dateNow.getDate() - 1)
-    const dayAgo = new Date(offset)
+    const dayAgo = new Date(dateNow)
+
+    dayAgo.setDate(dateNow.getDate() - 1)
 
     return [dayAgo, dateNow]
   }
