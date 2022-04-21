ebe71df0e3cb286a4be743e6d103fc63e3fee79d
Craig Harshbarger <pleek91@gmail.com>
2022-04-21 16:15:48 -0500

    Update the demo to support groups

--- demo/views/Heatmap.vue
+++ demo/views/Heatmap.vue
@@ -1,34 +1,51 @@
 <template>
   <div class="controls">
+    <span>Groups</span>
+    <input v-model="numberOfGroups" type="number">
     <span>Buckets</span>
     <input v-model="bucketAmount" type="number">
     <span>Opacity Range</span>
     <input v-model="bucketOpacityRange" type="number">
     <input v-model="color" type="color">
+  </div>
+  <div class="controls">
+    <select v-model="groupToAddTo">
+      <template v-for="(group, index) in groups" :key="group">
+        <option :value="index">
+          Group {{ index + 1 }}
+        </option>
+      </template>
+    </select>
+    <input v-model="itemsToAdd" type="number">
     <button type="button" @click="add">
-      Add Item
+      Add Items
     </button>
   </div>
   <Heatmap class="heatmap" v-bind="{ items, bucketAmount, bucketOpacityRange }" />
 </template>
 
 <script lang="ts" setup>
-  import { ref, Ref } from 'vue'
+  import { computed, ref, Ref, watch } from 'vue'
   import Heatmap from '../../src/components/Heatmap.vue'
   import { HeatmapItem } from '../../src/types'
 
   const oneWeekAgo = new Date(new Date().setDate(new Date().getDate() - 7))
   const now = new Date()
-  const items: Ref<HeatmapItem[]> = ref(new Array(50).fill(null).map(getHeatmapItem))
+  const items: Ref<HeatmapItem[]> = ref([])
   const bucketAmount = ref(30)
   const bucketOpacityRange = ref(4)
   const color = ref('#000000')
+  const numberOfGroups = ref(1)
+  const groupToAddTo = ref(0)
+  const itemsToAdd = ref(1)
 
-  function getHeatmapItem(): HeatmapItem {
+  const groups = computed(() => new Array(numberOfGroups.value).fill(null).map((item, index) => `Group ${index + 1}`))
+
+  function getHeatmapItem(group: string): HeatmapItem {
     return {
       id: getRandomString(),
       date: getRandomDate(oneWeekAgo, now),
-      group: 'test-item',
+      group,
     }
   }
 
@@ -41,8 +58,26 @@
   }
 
   function add(): void {
-    items.value.push(getHeatmapItem())
+    const group = groups.value[groupToAddTo.value]
+
+    addMany(group, itemsToAdd.value)
   }
+
+  function addMany(group: string, amount: number): void {
+    const newItems = new Array(amount).fill(null).map(() => getHeatmapItem(group))
+
+    items.value.push(...newItems)
+  }
+
+  function getHeatMapItems(): void {
+    groups.value.forEach(group => addMany(group, 50))
+  }
+
+  watch(numberOfGroups, () => {
+    items.value = []
+
+    getHeatMapItems()
+  }, { immediate: true })
 </script>
 
 <style lang="scss">
@@ -57,7 +92,7 @@
   padding: 8px;
 }
 
-.heatmap-row__bucket--test-item {
+.heatmap-row__bucket {
   background-color: v-bind(color);
 }
 </style>
