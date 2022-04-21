a1e5b61433c1565cde94a76c1e0230ce945c73df
Craig Harshbarger <pleek91@gmail.com>
2022-04-21 17:39:47 -0500

    Make sure extent uses the start date when creating a minimum range

--- src/utilities/extent.ts
+++ src/utilities/extent.ts
@@ -7,15 +7,24 @@ export function extentUndefined(extent: unknown[]): extent is [undefined, undefi
 
 export function getDateExtent<T extends Record<string, unknown>>(items: T[], accessor: DateExtentAccessor<T>): [Date, Date] {
   const response = extent(items, accessor)
-  const [start, end] = response
 
-  if (extentUndefined(response) || start == end) {
+  if (extentUndefined(response)) {
     const dateNow = new Date
     const dayAgo = new Date(dateNow)
 
     dayAgo.setDate(dateNow.getDate() - 1)
 
     return [dayAgo, dateNow]
+  }
+
+  const [start, end] = response
+
+  if (start === end) {
+    const dayAgo = new Date(start)
+
+    dayAgo.setDate(start.getDate() - 1)
+
+    return [dayAgo, start]
   }
 
   return response
