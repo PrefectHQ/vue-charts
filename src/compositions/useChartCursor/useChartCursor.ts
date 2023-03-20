345158409c86558095c4374ca9f3ad2296f4714f
Craig Harshbarger <pleek91@gmail.com>
2023-03-20 17:59:05 -0500

    Rename and allow for named cursors

---
+++ src/compositions/useChartCursor/useChartCursor.ts
@@ -0,0 +1,33 @@
+import { ref, Ref } from 'vue'
+
+export type UseChartCursor = {
+  cursor: Ref<Date | null>,
+}
+
+export function cursorFactory(): UseChartCursor {
+  const cursor = ref<Date | null>(null)
+
+  return {
+    cursor,
+  }
+}
+
+export function useNamedCursorFactory(): (name?: string) => UseChartCursor {
+  const cursors = new Map<string, UseChartCursor>()
+
+  function getCursorByName(name: string): UseChartCursor {
+    console.log('cursor', name)
+    if (cursors.has(name)) {
+      return cursors.get(name)!
+    }
+
+    const cursor = cursorFactory()
+    cursors.set(name, cursor)
+
+    return cursor
+  }
+
+  return (name: string = 'default') => getCursorByName(name)
+}
+
+export const useChartCursor = useNamedCursorFactory()
